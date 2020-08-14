2bcd2ceeef7fba7932341517c0806a636a47337b
nekomamoushi <germione.grangere@gmail.com>
2020-08-14 18:16:08 +0200

    feat: setup mongoose

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const MONGODB_HOST = process.env.MONGODB_HOST;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_URI = `mongodb+srv://${MONGODB_HOST}:${MONGODB_PASSWORD}@cluster0.vxrfi.azure.mongodb.net/todos?retryWrites=true&w=majority`;
 
 const todosRoutes = require('./routes/todos');
 
@@ -9,6 +14,13 @@ const app = express();
 
 app.use(API_VERSION + '/todos', todosRoutes);
 
-app.listen(process.env.PORT || 3000);
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((error) => {
+    console.log('Something went wrong with Mongoose');
+  });
 
 module.exports = app;
