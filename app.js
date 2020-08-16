04c0faaad76d3b95a0da6d1403f435c835b0a0f2
nekomamoushi <germione.grangere@gmail.com>
2020-08-16 16:32:16 +0200

    feat: handle cors issues

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const MONGODB_HOST = process.env.MONGODB_HOST;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-const MONGODB_URI = `mongodb+srv://${MONGODB_HOST}:${MONGODB_PASSWORD}@cluster0.vxrfi.azure.mongodb.net/todos?retryWrites=true&w=majority`;
+const MONGODB_URI =
+  `mongodb+srv://${MONGODB_HOST}:${MONGODB_PASSWORD}@cluster0.vxrfi.azure.mongodb.net/todos?retryWrites=true&w=majority`;
 
-const todosRoutes = require('./routes/todos');
+const todosRoutes = require("./routes/todos");
 
-const API_VERSION = '/api/v1';
+const API_VERSION = "/api/v1";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(API_VERSION + '/todos', todosRoutes);
+// Handle CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(API_VERSION + "/todos", todosRoutes);
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,7 +35,7 @@ mongoose
     app.listen(process.env.PORT || 3000);
   })
   .catch((error) => {
-    console.log('Something went wrong with Mongoose');
+    console.log("Something went wrong with Mongoose");
   });
 
 module.exports = app;
