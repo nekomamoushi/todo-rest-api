cd504fb444e95ec63b64c9fb38cd1ef000287903
nekomamoushi <germione.grangere@gmail.com>
2020-08-19 17:10:19 +0200

    feat: secure app with helmet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const { API_VERSION } = require('./config/index');
 const cors = require('./middlewares/cors');
@@ -6,6 +7,7 @@ const todosRoutes = require('./routes/todos');
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
