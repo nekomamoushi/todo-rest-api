e607864a35c8e51fd74fe4859f339e10fa498886
nekomamoushi <germione.grangere@gmail.com>
2020-08-21 12:20:12 +0200

    feat: add timestamp on todo creation

--- models/todo.js
+++ models/todo.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
-const todoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    require: true,
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    done: {
+      type: Boolean,
+      require: true,
+    },
   },
-  done: {
-    type: Boolean,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('todo', todoSchema);
